870e94399e30a01102aa332162e1e75fea1588b9
JaceyStew6 <r.pouchain@live.fr>
2024-02-20 17:09:35 +0100

    React: ajout exo 7

--- Formation_CDA/16_React/Exercices/Exercice07/exercice07/src/components/LoginComponent.jsx
+++ Formation_CDA/16_React/Exercices/Exercice07/exercice07/src/components/LoginComponent.jsx
@@ -4,11 +4,18 @@ const LoginComponent = (props) => {
 
     const {getValuesFromInputs} = props
 
+    const validUser = "Jacey"
+    const validPassword = "password"
     const usernameRef = useRef("")
     const passwordRef = useRef("")
 
     function getUserLogin() {
         getValuesFromInputs(usernameRef.current.value, passwordRef.current.value)
+        if (usernameRef.current.value != validUser || passwordRef.current.value != validPassword) {
+            console.log("Nom d'utilisateur ou mot de passe incorrect")
+        } else {
+            console.log("Bienvenue " + usernameRef.current.value )
+        }
     }
 
     return (
