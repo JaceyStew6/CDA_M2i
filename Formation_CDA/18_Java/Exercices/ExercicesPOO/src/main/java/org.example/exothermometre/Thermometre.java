29a344a9ffd725c59207922a933c8df8e512ee2f
JaceyStew6 <r.pouchain@live.fr>
2024-04-10 17:26:42 +0200

    Java: modification exo poo thermometre

--- Formation_CDA/18_Java/Exercices/ExercicesPOO/src/main/java/org.example/exothermometre/Thermometre.java
+++ Formation_CDA/18_Java/Exercices/ExercicesPOO/src/main/java/org.example/exothermometre/Thermometre.java
@@ -11,8 +11,52 @@ public class Thermometre {
         FAHRENHEIT,
     }
 
-    public Thermometre(double temperatureKelvin, UnitTemperature unit) {
-        this.temperatureKelvin = temperatureKelvin;
+
+//    public Thermometre(double temperature, UnitTemperature unit) {
+////        this.unit = unit;
+//        switch (unit){
+//            case KELVIN:
+//                this.temperatureKelvin = temperature;
+//                break;
+//            case CELSIUS:
+//                this.temperatureKelvin = temperature + 273.15;
+//                break;
+//            case FAHRENHEIT:
+//                this.temperatureKelvin = (temperature - 32) * 5/9 + 273.15;
+//                break;
+//            default:
+//                break;
+//        }
+//    }
+//
+//    public double getTemperatureKelvin(){
+//        return this.temperatureKelvin;
+//    }
+//     public double getTemperatureCelsius(){
+//        return this.temperatureKelvin - 273.15;
+//     }
+//
+//     public double getTemperatureFahrenheit(){
+//        return (this.temperatureKelvin - 273.15)* 9/5 + 32;
+//     }
+//
+//    public void setTemperature(double temperature, UnitTemperature unit) {
+//        switch (unit) {
+//            case KELVIN:
+//                this.temperatureKelvin = temperature;
+//                break;
+//            case CELSIUS:
+//                this.temperatureKelvin = temperature + 273.15;
+//                break;
+//            case FAHRENHEIT:
+//                this.temperatureKelvin = (temperature + 459.67) * 5/9;
+//                break;
+//        }
+//    }
+
+
+    public Thermometre(double temperature, UnitTemperature unit) {
+        this.temperatureKelvin = temperature;
         this.unit = unit;
     }
 
--- Formation_CDA/18_Java/Exercices/ExercicesPOO/src/main/java/org.example/exothermometre/main.java
+++ Formation_CDA/18_Java/Exercices/ExercicesPOO/src/main/java/org.example/exothermometre/main.java
@@ -3,11 +3,14 @@ package org.example.exothermometre;
 public class main {
     public static void main(String[] args) {
 
-        Thermometre thermometre1 = new Thermometre(2, Thermometre.UnitTemperature.KELVIN);
+        Thermometre thermometre1 = new Thermometre(2, Thermometre.UnitTemperature.CELSIUS);
         Thermometre thermometre2 = new Thermometre(300, Thermometre.UnitTemperature.CELSIUS);
         Thermometre thermometre3 = new Thermometre(50, Thermometre.UnitTemperature.FAHRENHEIT);
 
-        System.out.println("La température en degrés Kelvin est " + thermometre1.getTemperatureByUnit());
+/*        System.out.println("Kelvin " + thermometre1.getTemperatureKelvin());
+        System.out.println("Celsius " + thermometre1.getTemperatureCelsius());
+        System.out.println("Fahrenheit " + thermometre1.getTemperatureFahrenheit());*/
+
         System.out.println("Température initiale en Kelvin: "+ thermometre2.getTemperatureKelvin() +
                 " degrés. La température en degrés Celsius est " + thermometre2.getTemperatureByUnit());
         System.out.println("Température initiale en Kelvin: "+ thermometre3.getTemperatureKelvin() +
